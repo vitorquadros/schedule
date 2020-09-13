eb5c334f6a5085109f6abc181672789a02f12395
Vitor Quadros <viitorquadros@hotmail.com>
2020-09-13 19:35:45 -0300

    criar contato

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -22,3 +22,12 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa estar logado.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+  next();
+}
