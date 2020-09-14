7d91f91ebda3046cf5a2b3f202287d09a44f03b0
Vitor Quadros <viitorquadros@hotmail.com>
2020-09-14 18:39:09 -0300

    validação form frontend

---
+++ frontend/modules/Login.js
@@ -0,0 +1,36 @@
+import validator from 'validator';
+
+export default class Login {
+    constructor(formClass) {
+        this.form = document.querySelector(formClass);
+    }
+
+    init() {
+        this.events();
+    }
+
+    events() {
+        if (!this.form) return;
+        this.form.addEventListener('submit', e => {
+            e.preventDefault();
+            this.validate(e);
+        });
+    }
+
+    validate(e) {
+        const el = e.target;
+        const emailInput = el.querySelector('input[name="email"]');
+        const passwordInput = el.querySelector('input[name="password"]');
+        let error = false;
+
+        if(!validator.isEmail(emailInput.value)) {
+            alert('Email inválido.');
+            error = true;
+        }
+
+        if(passwordInput.value.length < 8 || passwordInput.value.length > 24) {
+            alert('Senha precisa ter entre 8 e 24 caracteres.');
+            error = true;
+        }
+    }
+}
